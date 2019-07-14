a2471cf76e05dddea68d5ef23c9ad7f5cfbf9cb8
Paulo dos Santos <paulo@fazendadosoftware.com>
2019-07-14 20:25:53 -0300

    current state

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -140,4 +140,14 @@ export default {
   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
     opacity 0
 
+  
+  .shadow-1
+  box-shadow 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)
+
+  .shadow-2
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)
+
+  .shadow-3
+    box-shadow 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)
+
 </style>
